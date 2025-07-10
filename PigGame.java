4fde67cd4658ff1cd39791add0d7c71fca3ce483
Samruddhi2003github <150898732+Samruddhi2003github@users.noreply.github.com>
2025-07-10 11:51:03 +0530

    Updated code

--- PigGame.java
+++ PigGame.java
@@ -1,25 +1,55 @@
-package com.aurionpro.practice;
+package com.aurionpro.day1;
 
 import java.util.Scanner;
 
 public class PigGame {
-	private static final int TARGET_SCORE = 20;
 
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
-		Score totalScore = new Score();
-		int turns = 0;
+		
+		startGame(scanner);
 
-		while (!totalScore.hasReachedLimit(TARGET_SCORE)) {
-			System.out.println("\nTurn " + (turns + 1));
-			PlayerTurn turn = new PlayerTurn(scanner);
-			int turnPoints = turn.play();
-			totalScore.add(turnPoints);
-			System.out.println("Total Score: " + totalScore.getValue());
-			turns++;
-		}
+		
 
-		System.out.println("You finished in " + turns + " turns. Game over!");
-		scanner.close();
 	}
+
+	private static void startGame(Scanner scanner) {
+		int lastTernPoints = 0;
+		int points = 0;
+		int turn = 0;
+		for (;;) {
+			System.out.println("Roll or Hold? (r/h)");
+			String choice = scanner.nextLine().toLowerCase();
+
+			if (choice.equals("h")) {
+				lastTernPoints += points;
+				points = 0;
+				turn++;
+				System.out.println("Your score points upto current term " + lastTernPoints);
+			} else {
+				int point = (int) (Math.random() * 6) + 1;
+				System.out.println("You got " + point);
+				if (point == 1) {
+					points = 0;
+					turn++;
+					System.out.println("You lose all points for the current turn");
+				} else {
+					points += point;
+					if (lastTernPoints + points >= 20) {
+						turn++;
+						System.out.println("You win the game with the points " + (lastTernPoints + points));
+						System.out.println("You take " + turn + " turns to win the game");
+						break;
+					}
+				}
+			}
+			if (turn > 5) {
+				System.out.println("Sorry, you lose the game");
+				break;
+			}
+		}
+		
+	}
+
 }
